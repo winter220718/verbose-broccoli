7e306cd54c4f7a9049bba5e5bff4155350a74a42
Gi7C-09 <Gi7C-09@Gi7A-27>
2022-12-05 14:25:01 +0900

    add banana feature

--- src/fruit/Banana.java
+++ src/fruit/Banana.java
@@ -3,4 +3,16 @@ package fruit;
 public class Banana {
 	int price;
 	int grade;
+	
+	public Banana() {
+		// TODO Auto-generated constructor stub
+		this(100, 100);
+	}
+	
+	public Banana(int price, int grade) {
+		this.price = price;
+		this.grade = grade;
+		
+	}
+
 }
